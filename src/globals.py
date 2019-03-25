4bda94aeb60738e2ad7b8809dae0117b58620317
Dov Bridger <dov@ef.technion.ac.il>
2019-03-25 11:52:33 +0200

    virtual dataset creates metadata

--- src/globals.py
+++ src/globals.py
@@ -1,13 +1,13 @@
 from os import path
 
 METADATA_FILE_NAME = 'metadata.txt'
-WINDOWS_ROOT_DIR = r"C:\checkouts\puzzle_gan_data\artifacts"
+METADATA_FOLDER_NAME = 'metadata'
+METADATA_DELIMITER = ';'
+WINDOWS_ROOT_DIR = r'C:\SHARE\checkouts\puzzle_gan_data'
+DATASET_NAME = 'virtual_puzzle_parts'
 MODEL_ROOT_DIR_NAME = "CNN_small_input"
-ROOT_OF_MODEL_DATA = path.join(WINDOWS_ROOT_DIR, MODEL_ROOT_DIR_NAME)
-TRAINING_DATA_PATH = path.join(ROOT_OF_MODEL_DATA, "train")
-TEMP_DATA_PATH = path.join(ROOT_OF_MODEL_DATA, "temp")
-VALIDATION_DATA_PATH = path.join(ROOT_OF_MODEL_DATA, "validation")
-TEST_DATA_PATH = path.join(ROOT_OF_MODEL_DATA, "test")
+ROOT_OF_MODEL_DATA = path.join(WINDOWS_ROOT_DIR, 'artifacts', MODEL_ROOT_DIR_NAME)
+TEST_DATA_PATH = path.join(WINDOWS_ROOT_DIR, 'datasets', DATASET_NAME, "test")
 FIGURES_FOLDER = path.join(ROOT_OF_MODEL_DATA, "figures")
 NAME_MAGIC = 'name='
 BURN_EXTENT_MAGIC = 'burn_extent='
