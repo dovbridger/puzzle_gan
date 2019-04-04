9a7e84294201c22470919dc5bf8510f42b2da9a9
Dov Bridger <dov@ef.technion.ac.il>
2019-04-04 09:46:23 +0300

    untrack .bat files

--- src/calc_diff2.bat
+++
@@ -1,1 +0,0 @@
-python calc_diff.py --dataset_name virtual_puzzle --model calc_diff --name g40_d30_b4_v --burn_extent 4 --dataroot ../../puzzle_gan_data/datasets/virtual_puzzle_parts --generator_window 8 --discriminator_window 32 --network_to_load batch1_burn4_post_40_virtual --network_load_epoch 30 --batchSize 1
