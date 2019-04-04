aef0ee5b5e2320d6a117070f87534c666f4f8525
Dov Bridger <dov@ef.technion.ac.il>
2019-04-04 15:17:59 +0300

    burn_by_mask

--- src/data/virtual_image.py
+++ src/data/virtual_image.py
@@ -8,11 +8,18 @@ from puzzle.java_utils import get_top_k_neighbors, get_java_diff_file, parse_3d_
 from globals import HORIZONTAL, VERTICAL, NAME_MAGIC, ORIENTATION_MAGIC
 
 class VirtualImage:
-    def __init__(self, path, opt, num_examples_accumulated):
+
+    opt = None
+
+    @staticmethod
+    def initialize(opt):
+        VirtualImage.opt = opt
+
+
+    def __init__(self, path, num_examples_accumulated):
         self.image_dir = os.path.dirname(path)
         self.name_horizontal, self.image_extension = os.path.splitext(os.path.basename(path))
         self.name_vertical = set_orientation_in_name(self.name_horizontal, VERTICAL)
-        self.opt = opt
 
         from data.virtual_puzzle_dataset import VirtualPuzzleDataset
         self.horizontal = VirtualPuzzleDataset.get_real_image(os.path.join(
@@ -20,18 +27,18 @@ class VirtualImage:
             self.name_horizontal + self.image_extension))
         self.vertical = self.horizontal.transpose(2, 1).flip(1)
         _, height, width = self.horizontal.shape
-        assert height % opt.part_size == 0 and width % opt.part_size == 0, \
+        assert height % VirtualImage.opt.part_size == 0 and width % VirtualImage.opt.part_size == 0, \
             "Image ({0}x{1} wasn't cropped to be a multiple of 'part_size'({2})".format(height, width,
-                                                                                        opt.part_size)
-        self.num_x_parts = int(width / opt.part_size)
-        self.num_y_parts = int(height / opt.part_size)
+                                                                                        VirtualImage.opt.part_size)
+        self.num_x_parts = int(width / VirtualImage.opt.part_size)
+        self.num_y_parts = int(height / VirtualImage.opt.part_size)
         self.num_horizontal_examples = self.count_pair_examples_in_image(self.num_x_parts,
                                                                          self.num_y_parts,
-                                                                         opt.num_false_examples)
+                                                                         VirtualImage.opt.num_false_examples)
         # x and y are reversed in vertical
         self.num_vertical_examples = self.count_pair_examples_in_image(self.num_y_parts,
                                                                        self.num_x_parts,
-                                                                       opt.num_false_examples)
+                                                                       VirtualImage.opt.num_false_examples)
         self.num_examples = self.num_horizontal_examples + self.num_vertical_examples
 
         self.num_examples_accumulated = self.num_examples + num_examples_accumulated
@@ -40,14 +47,14 @@ class VirtualImage:
     def get_neighbor_choices(self):
         num_parts = self.num_x_parts * self.num_y_parts
         parts_range = range(num_parts)
-        if self.opt.max_neighbor_rank <= 1:
+        if VirtualImage.opt.max_neighbor_rank <= 1:
             all_choices = [parts_range for part in range(num_parts)]
             return {HORIZONTAL: all_choices, VERTICAL: all_choices}
 
         puzzle_name = get_info_from_file_name(self.name_horizontal, NAME_MAGIC)
         original_diff_matrix3d = parse_3d_numpy_array_from_json(get_java_diff_file(puzzle_name,
                                                                                    burn_extent='0',
-                                                                                   part_size=self.opt.part_size))
+                                                                                   part_size=VirtualImage.opt.part_size))
         result = {HORIZONTAL: [], VERTICAL: []}
         for name in [self.name_horizontal, self.name_vertical]:
             metadata = self.get_metadata(name)
@@ -56,7 +63,7 @@ class VirtualImage:
             for part in range(num_parts):
                 current_part_choices = [part for (part, score) in
                                         get_top_k_neighbors(part, diff_matrix2d, metadata,
-                                                            self.opt.max_neighbor_rank, reverse=False)]
+                                                            VirtualImage.opt.max_neighbor_rank, reverse=False)]
                 adjacent = part + 1
 
                 # Remove true neighbor from that part's false neighbor choices (if exists)
@@ -90,11 +97,10 @@ class VirtualImage:
     def crop_part_from_image(self, image_array, num_columns, part):
         row = int(part / num_columns)
         column = part % num_columns
-        return image_array[:, row * self.opt.part_size: (row + 1) * self.opt.part_size,
-                           column * self.opt.part_size: (column + 1) * self.opt.part_size]
+        return image_array[:, row * VirtualImage.opt.part_size: (row + 1) * VirtualImage.opt.part_size,
+                           column * VirtualImage.opt.part_size: (column + 1) * VirtualImage.opt.part_size]
 
     def crop_pair_from_image(self, part1, part2, orientation):
-
         part1_tensor = self.get_part_from_image(part1, orientation)
         part2_tensor = self.get_part_from_image(part2, orientation)
         return torch.cat((part1_tensor, part2_tensor), 2)
@@ -117,13 +123,13 @@ class VirtualImage:
         num_columns = self.num_x_parts if orientation is HORIZONTAL else self.num_y_parts
         neighbor_choices = self.neighbor_choices[orientation]
         # The part that corresponds with relative_index
-        part_index = int(relative_index / (self.opt.num_false_examples + 1))
+        part_index = int(relative_index / (VirtualImage.opt.num_false_examples + 1))
         # The part in the original puzzle (after skipping the rightmost column)
         part1 = int(part_index * num_columns / (num_columns - 1))
         assert part1 % num_columns < num_columns - 1, "part1 was selected from rightmost column, sum tin wong"
         # Initialize part2 as the true neighbor
         part2 = part1 + 1
-        if relative_index % (self.opt.num_false_examples + 1) == 0:
+        if relative_index % (VirtualImage.opt.num_false_examples + 1) == 0:
             # A true example
             label = 1
         else:
