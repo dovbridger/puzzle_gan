8fbf18a6fda97d0297a5692138ee93ca2b6a2254
Dov Bridger <dov@ef.technion.ac.il>
2019-04-06 18:22:40 +0300

    implemented generator mask

--- src/utils/network_utils.py
+++ src/utils/network_utils.py
@@ -17,8 +17,13 @@ def get_centered_window_indexes(initial_size, required_window_size):
     window_end = window_start + required_window_size
     return (window_start, window_end)
 
-def get_generator_mask(opt):
 
+def get_generator_mask(input_size, generator_window, burn_extent):
+    generator_mask = torch.zeros(input_size)
+    centere_window_width_indexes = get_centered_window_indexes(input_size[1], generator_window)
+    generator_mask[burn_extent: -burn_extent,
+                   centere_window_width_indexes[0] : centere_window_width_indexes[1]] = 1
+    return generator_mask
 
 def get_network_file_name(which_epoch, name):
     return'%s_net_%s.pth' % (which_epoch, name)
